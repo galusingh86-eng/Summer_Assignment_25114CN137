3426ffb3e4596763dada4bd58f581cf1fb3ce0f4
galusingh86-eng <galusingh86@gmail.com>
2026-06-04 14:26:03 +0530

    Day-4 uploaded

---
+++ Day-4/problem_15.java
@@ -0,0 +1,28 @@
+// write a progrm to check armstrong number.
+
+import java.util.Scanner;
+
+public class problem_15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int sum = 0, digit;
+
+        while (num > 0) {
+            digit = num % 10;
+            sum = sum + (digit * digit * digit);
+            num = num / 10;
+        }
+
+        if (sum == original)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not an Armstrong Number");
+
+        sc.close();
+    }
+}
