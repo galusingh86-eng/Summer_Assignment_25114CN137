d449524ea3fcfcf99d6b62d3b1ead55654b32bcf
galusingh86-eng <galusingh86@gmail.com>
2026-06-09 18:04:12 +0530

    Day-9 uploaded

---
+++ Day-9/problem_34.java
@@ -0,0 +1,14 @@
+//write a program to print reverse number triangle.
+
+public class problem_34{
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day-9/problem_35.java
@@ -0,0 +1,14 @@
+// write a program to print repeated character pattern.
+
+public class problem_35 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char)(64 + i) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
