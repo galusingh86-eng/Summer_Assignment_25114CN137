119e744918501fb62850b4a42544df90cb1c6997
galusingh86-eng <galusingh86@gmail.com>
2026-06-10 21:51:33 +0530

    Day-10 uploaded

---
+++ Day-10/problem_38.java
@@ -0,0 +1,29 @@
+// write a program to print reverse star pyramid.
+
+import java.util.Scanner;
+
+public class problem_38 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
---
+++ Day-10/problem_40.java
@@ -0,0 +1,27 @@
+// write  a program to print character pyramid.
+
+public class problem_40 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print increasing characters
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch);
+            }
+
+            // Print decreasing characters
+            for (char ch = (char)('A' + i - 2); ch >= 'A'; ch--) {
+                System.out.print(ch);
+            }
+
+            System.out.println();
+        }
+    }
+}
