cc1bb533c1a831fd7d53ae418ce3b390f8b0a02f
galusingh86-eng <galusingh86@gmail.com>
2026-06-13 19:57:48 +0530

    Day-13 upoaded

---
+++ Day-13/problem_49.java
@@ -0,0 +1,27 @@
+// write a program to input and display array.
+
+import java.util.Scanner;
+
+public class problem_49 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        // Input array elements
+        System.out.println("Enter 5 elements of array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Display array elements
+        System.out.println("Array elements are:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
+
+
